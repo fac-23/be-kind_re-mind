349d7655312546a72d32825fbffb0d51d39504ce
miahbates <miahbates@icloud.com>
2022-03-02 16:38:41 +0000

    able to get medications from db and display on medication page
    Co-authored-by: Adam 31065348+adam8-9@users.noreply.github.com

--- components/medicineBox.jsx
+++ components/medicineBox.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import Link from "next/link";
 
-export default function MedicineBox({ medicineObj }) {
+export default function MedicineBox({ medicineObj, medicationInfo }) {
+  console.log("medication info", medicationInfo);
   return (
     <Link href="/medication">
       <a>
         <div className="box">
           <h2>Your medicines 💊</h2>
-          <p>
-            {medicineObj.remaining} / {medicineObj.total} {medicineObj.drug}
-          </p>
+          <ul>
+            {medicationInfo.map((medication) => (
+              <li className="med">
+                <p>{medication.medname}</p>
+                <p>{medication.medicationtype}</p>
+                <p>
+                  {medication.meddose}
+                  {medication.units}
+                </p>
+                <p>{medication.medtime}</p>
+                <p>{medication.notes}</p>
+              </li>
+            ))}
+          </ul>
         </div>
       </a>
     </Link>
   );
 }
+
+/* <p>
+            {medicineObj.remaining} / {medicineObj.total} {medicineObj.drug}
+          </p> */
