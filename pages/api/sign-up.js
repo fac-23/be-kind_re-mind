6e962bba95a0a733cc6fde117639a5be4743f6c4
Oli Jones <78092825+duckRabbitPy@users.noreply.github.com>
2022-03-03 08:45:51 +0000

    import cookie package for signup

--- pages/api/sign-up.js
+++ pages/api/sign-up.js
@@ -1,6 +1,7 @@
 import { cookie_options, saveSession } from "../../auth";
 import { createUser } from "../../database/model";
 import bcrypt from "bcryptjs";
+import Cookies from "cookies";
 
 export function hashPassword(password) {
   return bcrypt.hash(password, 10);
