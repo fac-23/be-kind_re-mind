76402a5c3d2fb349cf3b3c64b2322b98302de54f
miahbates <miahbates@icloud.com>
2022-03-06 17:40:44 +0000

    Line 6 login.jsx changed styled.a to styled.button as that was the original html component

--- pages/login.jsx
+++ pages/login.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import styled from "styled-components";
 import Image from "next/image";
 
-const StyledLogin = styled.a`
+const StyledLogin = styled.button`
   background: #009444;
 
   & :hover {
