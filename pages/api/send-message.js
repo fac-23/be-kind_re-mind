12715ca28f0edc6daf5b57bbd31c053d5dbca5e3
Oli Jones <78092825+duckRabbitPy@users.noreply.github.com>
2022-03-08 16:18:28 +0000

    Update send-message.js

--- pages/api/send-message.js
+++ pages/api/send-message.js
@@ -1,25 +1,35 @@
+import { getAllMeds } from "../../database/model";
+
 export default async function send_message(req, res) {
   switch (req.method) {
-    case "POST": {
-      const accountSid = process.env.TWILIO_SID;
-      const authToken = process.env.TWILIO_AUTH_TOKEN;
+    case "GET": {
+      const record = await getAllMeds(1);
 
-      const twilio = require("twilio");
-      const client = new twilio(accountSid, authToken);
+      const dailySchedule = record.map((obj) => {
+        return obj.medtime;
+      });
 
-      client.messages
-        .create({
-          body: "Hello from Node",
-          to: process.env.DEV_NUM,
-          from: process.env.TWILIO_PHONE_NUM,
-        })
-        .then((message) => {
-          console.log(message.sid);
-          res.redirect(303, "/home");
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+      console.log(dailySchedule);
+
+      //   const accountSid = process.env.TWILIO_SID;
+      //   const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+      //   const twilio = require("twilio");
+      //   const client = new twilio(accountSid, authToken);
+
+      //   client.messages
+      //     .create({
+      //       body: "Hello from Node",
+      //       to: process.env.DEV_NUM,
+      //       from: process.env.TWILIO_PHONE_NUM,
+      //     })
+      //     .then((message) => {
+      //       console.log(message.sid);
+      //       res.redirect(303, "/home");
+      //     })
+      //     .catch((error) => {
+      //       console.log(error);
+      //     });
 
       res.redirect(303, "/home");
     }
