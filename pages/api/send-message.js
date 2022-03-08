4ea9ac1477ea995b7067d67fbcb7e93f0c475492
miahbates <miahbates@icloud.com>
2022-03-08 17:48:41 +0000

    pushed up changes for the day - explore moving try/catch to check if the action key is sent back in the header
    Co-authored-by: Oli  <78092825+duckRabbitPy@users.noreply.github.com>

--- pages/api/send-message.js
+++ pages/api/send-message.js
@@ -2,7 +2,23 @@ import { getAllMeds } from "../../database/model";
 
 export default async function send_message(req, res) {
   switch (req.method) {
-    case "GET": {
+    case "POST": {
+      const { ACTION_KEY } = req.headers.authorization.split(" ")[1];
+
+      //       try {
+      //         if (ACTION_KEY === process.env.APP_KEY) {
+      //           console.log("Ready to send post request");
+      //           res.status(200).json({ success: "true" });
+      //         } else {
+      //           res.status(401);
+      //         }
+      //       } catch (err) {
+      //         res.status(500);
+      //       }
+      //       res.status(200);
+      //     }
+      //   }
+
       const record = await getAllMeds(1);
 
       const dailySchedule = record.map((obj) => {
