32cd3ce8bbf154a7c3d68d3f89549551af6b6af9
Oli Jones <78092825+duckRabbitPy@users.noreply.github.com>
2022-03-09 12:50:00 +0000

    Uncomment fake send
    Co-Authored-By: Adam <31065348+adam8-9@users.noreply.github.com>
    Co-Authored-By: Miah Elizabeth Bates <78933903+miahbates@users.noreply.github.com>
    Co-Authored-By: Juliette Orpen <53294140+julietteorpen@users.noreply.github.com>

--- pages/api/fake-send-message.js
+++ pages/api/fake-send-message.js
@@ -1,20 +1,22 @@
-// export default async function send_message(req, res) {
-//   switch (req.method) {
-//     case "POST": {
-//       console.log("fake endpoint reached");
-//       const { ACTION_KEY } = req.headers.authorization.split(" ")[1];
+export default async function send_message(req, res) {
+  switch (req.method) {
+    case "POST": {
+      console.log("fake endpoint reached");
+      const { ACTION_KEY } = req.headers.authorization.split(" ")[1];
 
-//       try {
-//         if (ACTION_KEY === process.env.APP_KEY) {
-//           console.log("Ready to send post request");
-//           res.status(200).json({ success: "true" });
-//         } else {
-//           res.status(401);
-//         }
-//       } catch (err) {
-//         res.status(500);
-//       }
-//       res.status(200);
-//     }
-//   }
-// }
+      try {
+        if (ACTION_KEY === process.env.APP_KEY) {
+          console.log(ACTION_KEY);
+          console.log(process.env.APP_KEY);
+          console.log("Ready to send post request");
+          res.status(200).json({ success: "true" });
+        } else {
+          res.status(401);
+        }
+      } catch (err) {
+        res.status(500);
+      }
+      res.status(200);
+    }
+  }
+}
