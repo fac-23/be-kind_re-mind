9c6147441f0216dce5a783b12e67c97ee1d47934
miahbates <miahbates@icloud.com>
2022-03-09 12:09:41 +0000

    removed github actions file using cron instead of github action
    * Co-authored-by: Oli  <78092825+duckRabbitPy@users.noreply.github.com>
    * Co-authored-by: Adam <31065348+adam8-9@users.noreply.github.com>
    * Co-authored-by: Miah <78933903+miahbates@users.noreply.github.com>
    * Co-authored-by: Juliette <53294140+julietteorpen@users.noreply.github.com>

--- pages/api/send-message.js
+++ pages/api/send-message.js
@@ -5,71 +5,64 @@ export default async function send_message(req, res) {
     case "POST": {
       const { ACTION_KEY } = req.headers.authorization.split(" ")[1];
 
-      //       try {
-      //         if (ACTION_KEY === process.env.APP_KEY) {
-      //           console.log("Ready to send post request");
-      //           res.status(200).json({ success: "true" });
-      //         } else {
-      //           res.status(401);
-      //         }
-      //       } catch (err) {
-      //         res.status(500);
-      //       }
-      //       res.status(200);
-      //     }
-      //   }
+      try {
+        if (ACTION_KEY === process.env.APP_KEY) {
+          console.log("Ready to send post request");
+          const record = await getAllMeds(1);
 
-      const record = await getAllMeds(1);
-
-      const dailySchedule = record.map((obj) => {
-        return obj.medtime;
-      });
-
-      console.log("array of time", dailySchedule);
-
-      // break down daily schedule into 2 digit num
-
-      let formatedDailySchedule = dailySchedule.map((element) => {
-        const split = element.split(":");
-        const hour = parseInt(split[0]);
-        return hour;
-        // [13, 15, 18]
-      });
-      console.log("line 23", formatedDailySchedule);
-
-      formatedDailySchedule = [13, 16, 17];
-      console.log("line 25", formatedDailySchedule);
-
-      // get current date using get.date/hours
-
-      let currentTime = new Date().getHours();
-      console.log("time", parseInt(currentTime));
-
-      if (formatedDailySchedule.includes(currentTime)) {
-        const accountSid = process.env.TWILIO_SID;
-        const authToken = process.env.TWILIO_AUTH_TOKEN;
-
-        const twilio = require("twilio");
-        const client = new twilio(accountSid, authToken);
-
-        client.messages
-          .create({
-            body: "You have a medication reminder from Be-Kind Re-Mind",
-            to: process.env.DEV_NUM,
-            from: process.env.TWILIO_PHONE_NUM,
-          })
-          .then((message) => {
-            console.log("message sent", message.sid);
-            res.status(200);
-          })
-          .catch((error) => {
-            console.log(error);
+          const dailySchedule = record.map((obj) => {
+            return obj.medtime;
           });
+
+          console.log("array of time", dailySchedule);
+
+          // break down daily schedule into 2 digit num
+
+          let formatedDailySchedule = dailySchedule.map((element) => {
+            const split = element.split(":");
+            const hour = parseInt(split[0]);
+            return hour;
+            // [13, 15, 18]
+          });
+
+          formatedDailySchedule = [12, 13, 14];
+
+          // get current date using get.date/hours
+          let currentTime = new Date().getHours();
+
+          if (formatedDailySchedule.includes(currentTime)) {
+            const accountSid = process.env.TWILIO_SID;
+            const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+            const twilio = require("twilio");
+            const client = new twilio(accountSid, authToken);
+
+            client.messages
+              .create({
+                body: "You have a medication reminder from Be-Kind Re-Mind",
+                to: process.env.DEV_NUM,
+                from: process.env.TWILIO_PHONE_NUM,
+              })
+              .then((message) => {
+                console.log("message sent", message.sid);
+                res.status(200);
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          }
+          res.status(200).json({ success: "true" });
+        } else {
+          res.status(401);
+        }
+      } catch (err) {
+        res.status(500);
       }
-
-      // compare and if matching then send twilio
-
       res.status(200);
     }
   }
+
+  // compare and if matching then send twilio
+
+  res.status(200);
 }
