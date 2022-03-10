e70ad139a908b36f5141d7f03ff7467d2df53c93
miahbates <miahbates@icloud.com>
2022-03-10 10:51:33 +0000

    don't use this

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,13 @@
 import Document from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
-// generate styled components to render during server side rendering
-
+// Generate styled components to render during server side rendering.
+// Styled-components creates an instance of ServerStyleSheet This stylesheet
+// retrieves any styles found in all the components
+// This passses down to the html template
+// Sheets.collect styles collects all of the styles from the app
+// sheets.getElement generates the style tag, this is returned as props
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
@@ -28,5 +33,22 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+  // Put font styles in document
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;1,700&family=Poppins:wght@300;700&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
